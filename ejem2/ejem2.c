79e01e692cf6f65c2abce2e7de898f515f9f7360
mathuu-21 <alan.mateo2112@gmail.com>
2024-09-27 15:10:46 -0500

    enter

---
+++ ejem2/ejem2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    
+    int nums [5];
+for (int i=0; i<6; i++)
+ {
+    printf("Ingrese el elemento %d: \n",i);
+    scanf("%f", &nums[i]);
+ }    
+    return 0;
+}
